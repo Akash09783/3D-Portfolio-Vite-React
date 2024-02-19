07dfe2b09aba171caa33445c698740e88182d4f9
Akash <kumawat.aakash515@gmail.com>
2024-02-19 12:37:17 +0530

    navbar design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-
-import './App.css'
+import { BrowserRouter } from "react-router-dom"
+import { About,Contact,Experience,Feedbacks,Hero, Navbar,Tech,Works,StarsCanvas } from "./components"
 
 function App() {
 
 
   return (
     <>
-<div>hii</div>
+<BrowserRouter>
+  <div className="relative z-0 bg-primary">
+  <div className="bg-hero-pattern bg-cover bg-no-repeat">
+  <Navbar/>
+  <Hero/>
+  </div>
+<About/>
+<Experience/>
+<Tech/>
+<Works/>
+<Feedbacks/>
+<div className="relative z-0">
+<Contact/>
+<StarsCanvas/>
+
+</div>
+
+  </div>
+</BrowserRouter>
     </>
   )
 }
