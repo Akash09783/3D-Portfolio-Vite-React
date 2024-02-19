101d8eaa7a8261ce7cb9051f52389de88298ace2
Akash <kumawat.aakash515@gmail.com>
2024-02-19 14:38:59 +0530

    added hero section 3D Model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w7x1 mx-auto flex 
+    flex-row items-start gap-5 `}
+      >
+        <div className="flex flex-col justify-center items-center mt-4">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
 
-export default Hero
+        <div className={`${styles.heroHeadText}text-white`}>
+          <h1>
+            Hi, I'm <span className="text-[#915eff]">Akash</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop 3D Visuals, User Interfaces,{" "}
+            <br className="sm:block hidden" /> Frontend And Backend Web
+            Applications.
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
+  );
+};
+
+export default Hero;
